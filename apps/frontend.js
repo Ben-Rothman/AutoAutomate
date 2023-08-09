ea1986c79e3d8b03627a61146c9c52a3f5a77735
Ben-Rothman <benrothman12@gmail.com>
2023-08-09 12:27:40 -0400

    Outline

--- apps/frontend.js
+++ apps/frontend.js
@@ -1,28 +1,81 @@
-let recordingData = [];
+document.addEventListener('DOMContentLoaded', function () {
+    const startButton = document.getElementById('startRecording');
+    const stopButton = document.getElementById('stopRecording');
+    const downloadPythonButton = document.getElementById('downloadPython');
+    
+    startButton.addEventListener('click', startRecording);
+    stopButton.addEventListener('click', stopRecording);
+    downloadPythonButton.addEventListener('click', downloadPythonFile);
+});
 
 function startRecording() {
-    // Send a request to the backend to start recording
-    // You can use XMLHttpRequest or Fetch API here
+    fetch('/start_recording', {
+        method: 'POST'
+    })
+    .then(response => response.json())
+    .then(data => console.log(data.message)) // Display a success message
+    .catch(error => console.error('Error starting recording:', error));
 }
 
 function stopRecording() {
-    // Send a request to the backend to stop recording
-    // You can use XMLHttpRequest or Fetch API here
+    fetch('/stop_recording', {
+        method: 'POST'
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Process the recorded data, which is available in data.data
+        console.log('Recorded data:', data.data);
+    })
+    .catch(error => console.error('Error stopping recording:', error));
 }
 
 function downloadPythonFile() {
-    const data = "Python code here"; // Replace with actual Python code
-    const blob = new Blob([data], { type: 'text/plain' });
+    fetch('/stop_recording', {
+        method: 'POST'
+    })
+    .then(response => response.json())
+    .then(data => {
+        const pythonCode = generatePythonCode(data.data); // Call a function to generate Python code
+        downloadFile('recorded.py', pythonCode); // Call a function to initiate the download
+    })
+    .catch(error => console.error('Error stopping recording:', error));
+}
+
+function generatePythonCode(recordedData) {
+    let pythonCode = '';
+    for (const action of recordedData) {
+        const [x, y, eventType] = action;
+        switch (eventType) {
+            case 'mousemove':
+                pythonCode += `pyautogui.moveTo(${x}, ${y})\n`;
+                break;
+            case 'mousedown':
+                pythonCode += `pyautogui.mouseDown(${x}, ${y})\n`;
+                break;
+            case 'mouseup':
+                pythonCode += `pyautogui.mouseUp(${x}, ${y})\n`;
+                break;
+            case 'scroll':
+                const [dx, dy] = action.slice(2);
+                pythonCode += `pyautogui.scroll(${dx}, ${dy})\n`;
+                break;
+            case 'keypress':
+                const key = action[0];
+                pythonCode += `pyautogui.press('${key}')\n`;
+                break;
+            // Handle other cases as needed
+        }
+    }
+    return pythonCode;
+}
+
+function downloadFile(filename, content) {
+    const blob = new Blob([content], { type: 'text/plain' });
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
-    a.download = 'recorded.py';
+    a.download = filename;
     document.body.appendChild(a);
     a.click();
     window.URL.revokeObjectURL(url);
-}
-
-function downloadExe() {
-    // Send a request to the backend to compile the Python file to EXE
-    // You can use XMLHttpRequest or Fetch API here
 }
--- apps/recording.py
+++ apps/recording.py
@@ -1,45 +1,60 @@
 from flask import Flask, request, jsonify
-import pyautogui
 from pynput.mouse import Listener as MouseListener
 from pynput.keyboard import Listener as KeyboardListener
 import threading
 
 app = Flask(__name__)
 recording_data = []
+mouse_listener = None
+keyboard_listener = None
+recording_lock = threading.Lock()
 
 def on_mouse_move(x, y, dx, dy):
-    recording_data.append((x, y, "mousemove"))
+    with recording_lock:
+        recording_data.append((x, y, "mousemove"))
 
 def on_click(x, y, button, pressed):
-    action = "mousedown" if pressed else "mouseup"
-    recording_data.append((x, y, action))
+    with recording_lock:
+        action = "mousedown" if pressed else "mouseup"
+        recording_data.append((x, y, action))
 
 def on_scroll(x, y, dx, dy):
-    recording_data.append((x, y, f"scroll({dx},{dy})"))
+    with recording_lock:
+        recording_data.append((x, y, f"scroll({dx},{dy})"))
 
 def on_press(key):
-    try:
-        recording_data.append((key.char, "keypress"))
-    except AttributeError:
-        recording_data.append((str(key), "keypress"))
+    with recording_lock:
+        try:
+            recording_data.append((key.char, "keypress"))
+        except AttributeError:
+            recording_data.append((str(key), "keypress"))
 
 def start_recording():
-    with MouseListener(on_move=on_mouse_move, on_click=on_click, on_scroll=on_scroll) as mouse_listener:
+    global mouse_listener, keyboard_listener
+    with MouseListener(on_move=on_mouse_move) as mouse_listener:
         with KeyboardListener(on_press=on_press) as keyboard_listener:
             mouse_listener.join()
             keyboard_listener.join()
 
 @app.route('/start_recording', methods=['POST'])
 def start_recording_endpoint():
-    recording_data.clear()
-    threading.Thread(target=start_recording).start()
+    global mouse_listener, keyboard_listener
+    with recording_lock:
+        recording_data.clear()
+        threading.Thread(target=start_recording).start()
     return jsonify({'message': 'Recording started'})
 
 @app.route('/stop_recording', methods=['POST'])
 def stop_recording_endpoint():
-    # Stop recording and return the recording data
-    # You may need to handle stopping the listeners properly
-    return jsonify({'data': recording_data})
+    global mouse_listener, keyboard_listener
+    with recording_lock:
+        data_to_return = recording_data.copy()
+        recording_data.clear()
+        if mouse_listener:
+            mouse_listener.stop()
+        if keyboard_listener:
+            keyboard_listener.stop()
+    return jsonify({'data': data_to_return})
 
 if __name__ == '__main__':
     app.run()
